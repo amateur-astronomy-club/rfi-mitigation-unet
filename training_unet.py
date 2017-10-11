bc7a462401218a62bd991e9332afe31c7cd0864e
rahulsnkr <rahulsnkr@gmail.com>
2017-10-11 22:15:03 +0530

    Added complete documentation

--- training_unet.py
+++ training_unet.py
@@ -1,16 +1,21 @@
 from scripts.radio_util import DataProvider
 from tf_unet import unet
 import glob
-files = glob.glob('/home/aditi/bgs_example_data/seek_cache/*')
+files = glob.glob('./bgs_example_data/seek_cache/*')
 data_provider = DataProvider(600, files)
 
+# Initializing the network. We can change the hyper-parameters to adjust the performance of our network. 
 net = unet.Unet(channels=data_provider.channels, 
                 n_class=data_provider.n_class, 
                 layers=3, 
                 features_root=64,
                 cost_kwargs=dict(regularizer=0.001),
                 )
+                
+# Initializing the trainer to the network
 trainer = unet.Trainer(net, optimizer="momentum", opt_kwargs=dict(momentum=0.2))
+
+# Training the network using the data and exporting the processed image to the specified path
 path = trainer.train(data_provider, "./unet_trained_bgs_example_data", 
                      training_iters=32, 
                      epochs=5, 
